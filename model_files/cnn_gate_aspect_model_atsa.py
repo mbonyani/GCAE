8727d9d517b1c4507df4ad999a22ff7602d25dcd
Mahdi <44380968+mbonyani@users.noreply.github.com>
2021-11-29 22:26:39 +0330

    Update cnn_gate_aspect_model_atsa.py

--- model_files/cnn_gate_aspect_model_atsa.py
+++ model_files/cnn_gate_aspect_model_atsa.py
@@ -1,6 +1,59 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
+
+from torch.nn import init
+
+
+class AFT_FULL(nn.Module):
+
+    def __init__(self, d_model,n=49,simple=False):
+
+        super(AFT_FULL, self).__init__()
+        self.fc_q = nn.Linear(d_model, d_model)
+        self.fc_k = nn.Linear(d_model, d_model)
+        self.fc_v = nn.Linear(d_model,d_model)
+        if(simple):
+            self.position_biases=torch.zeros((n,n))
+        else:
+            self.position_biases=nn.Parameter(torch.ones((n,n)))
+        self.d_model = d_model
+        self.n=n
+        self.sigmoid=nn.Sigmoid()
+
+        self.init_weights()
+
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.kaiming_normal_(m.weight, mode='fan_out')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal_(m.weight, std=0.001)
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+
+    def forward(self, input):
+
+        bs, n,dim = input.shape
+
+        q = self.fc_q(input) #bs,n,dim
+        k = self.fc_k(input).view(1,bs,n,dim) #1,bs,n,dim
+        v = self.fc_v(input).view(1,bs,n,dim) #1,bs,n,dim
+        
+        numerator=torch.sum(torch.exp(k+self.position_biases.view(n,1,-1,1))*v,dim=2) #n,bs,dim
+        denominator=torch.sum(torch.exp(k+self.position_biases.view(n,1,-1,1)),dim=2) #n,bs,dim
+
+        out=(numerator/denominator) #n,bs,dim
+        out=self.sigmoid(q)*(out.permute(1,0,2)) #bs,n,dim
+
+        return out
 
 
 class CNN_Gate_Aspect_Text(nn.Module):
@@ -32,7 +85,7 @@ class CNN_Gate_Aspect_Text(nn.Module):
 
         self.fc1 = nn.Linear(len(Ks)*Co, C)
         self.fc_aspect = nn.Linear(100, Co)
-
+        self.aft_full = AFT_FULL(d_model=1, n=Co)
 
     def forward(self, feature, aspect):
         feature = self.embed(feature)  # (N, L, D)
@@ -50,7 +103,7 @@ class CNN_Gate_Aspect_Text(nn.Module):
         x = [i*j for i, j in zip(x, y)]
 
         # pooling method
-        x = [F.max_pool1d(i, i.size(2)).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
+        x = [self.aft_full(F.max_pool1d(i, i.size(2))).squeeze(2) for i in x]  # [(N,Co), ...]*len(Ks)
         # x = [F.adaptive_max_pool1d(i, 2) for i in x]
         # x = [i.view(i.size(0), -1) for i in x]
 
